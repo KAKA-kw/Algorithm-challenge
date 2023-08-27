8a2db6afa50bd0dee0bd979a712866a6636998c3
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-08-28 01:17:10 +0900

    feat: 백준 1874번 스택 문제 풀이 추가

---
+++ jiyoung00000/2주차/bj_(10828)스택(stack).py
@@ -0,0 +1,29 @@
+# stack 풀이
+
+import sys # input()보다 빠르게 입력받기 위함
+n = int(sys.stdin.readline()) # 명령의 수
+
+stack=[] 
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0]=='push':
+        stack.append(command[1])
+    elif command[0]=='pop':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack)==0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[-1])
