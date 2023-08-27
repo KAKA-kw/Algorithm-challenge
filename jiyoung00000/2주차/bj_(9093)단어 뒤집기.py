380f0300b3e126571209b8288bf527d46c9cb5e6
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-08-28 01:18:28 +0900

    feat: 백준 9093번 단어 뒤집기 문제 풀이 추가

---
+++ jiyoung00000/2주차/bj_(9093)단어 뒤집기.py
@@ -0,0 +1,16 @@
+# deque 풀이
+from collections import deque
+import sys # input()보다 빠르게 입력받기 위함
+
+n = int(sys.stdin.readline()) # 문장의 수
+deq = deque()
+
+for i in range(n):
+    words = sys.stdin.readline().split() # 문장을 단어로 나누기
+    
+    for word in words:
+        if word != " ":
+            deq.extend(word)
+            while deq:
+                print(deq.pop(), end="")
+            print(" ", end="")
