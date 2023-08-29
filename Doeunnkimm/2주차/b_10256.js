8a97a080f2a614ec4792ff574353817c3825d16f
Doeunnkimm <doeunnkimm@gmail.com>
2023-08-29 12:16:51 +0900

    fix: 백준 10256(돌연변이) 문제 풀이 수정 (성공)

--- Doeunnkimm/2주차/b_10256.js
+++ Doeunnkimm/2주차/b_10256.js
@@ -1,63 +1,37 @@
-// const input = require('fs').readFileSync('b_10256.txt').toString().trim().split('\n')
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+const input = require('fs').readFileSync('./input/b_10256.txt').toString().trim().split('\n')
 
-function getMarkerArray(marker) {
-  const arr = []
+function getMarkerSet(marker) {
+  const set = new Set()
 
-  for (let i = 0; i < marker.length - 1; i++) {
-    const a = marker[i + 1]
-    const b = marker[i]
-
-    const first = marker.substring(0, i)
-    const third = marker.substring(i + 2, marker.length)
-    arr.push(first + a + b + third)
+  for (let i = 0; i <= marker.length; i++) {
+    for (let j = 0; j <= marker.length; j++) {
+      const reversed_second = marker.slice(i, j).split('').reverse().join('')
+      set.add(marker.slice(0, i) + reversed_second + marker.slice(j))
+    }
   }
-
-  for (let i = 0; i < marker.length - 2; i++) {
-    const a = marker[i + 1] + marker[i + 2]
-    const b = marker[i]
-
-    const first = marker.substring(0, i)
-    const third = marker.substring(i + 3, marker.length)
-    arr.push(first + a + b + third)
-  }
-
-  for (let i = 3; i < marker.length; i++) {
-    const a = marker[i]
-    const b = marker[i - 1] + marker[i - 2]
-
-    const first = marker.substring(0, i - 2)
-    arr.push(first + a + b)
-  }
-
-  arr.push(marker.split('').reverse().join(''))
-  arr.push(marker)
-  return [...new Set(arr)]
-}
-
-function getMarkerInDNACount(DNA, markerMap) {
-  let count = 0
-  markerMap.forEach((marker) => {
-    if (DNA.includes(marker)) count++
-  })
-  return count
+  return set
 }
 
 function solution() {
-  input.shift()
-  var splitInput = []
+  const test_count = input.shift()
+  const case_arr = []
 
-  for (let i = 0; i < input.length; i += 3) {
-    splitInput.push(input.slice(i, i + 3))
+  // 테스트 케이스 구분
+  for (let i = 0; i < input.length - 1; i += 3) {
+    case_arr.push(input.slice(i + 1, i + 3))
   }
 
-  const sol = []
-  splitInput.forEach((arr) => {
-    const DNA = arr[1]
-    const Marker = arr[2]
-    sol.push(getMarkerInDNACount(DNA, getMarkerArray(Marker)))
-  })
-  console.log(sol.join('\n'))
+  // 테스트 케이스만큼 output 도출
+  for (let i = 0; i < test_count; i++) {
+    let count = 0
+    const [dna, marker] = case_arr[i]
+    const markerSet = getMarkerSet(marker)
+
+    for (let j = 0; j < dna.length - marker.length + 1; j++) {
+      if (markerSet.has(dna.slice(j, j + marker.length))) count++
+    }
+    console.log(count)
+  }
 }
 
 solution()
