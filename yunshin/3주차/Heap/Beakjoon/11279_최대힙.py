aa9c4b16960c1a40a344544a72fd3d501e06b8d1
yunshin <2younsin@gmail.com>
2023-08-31 13:21:48 +0900

    feat: 백준 11279 문제풀이 추가

---
+++ yunshin/3주차/Heap/Beakjoon/11279_최대힙.py
@@ -0,0 +1,14 @@
+import sys
+import heapq as hq
+
+n = int(sys.stdin.readline())
+heap = []
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if heap:
+            print((-1)*hq.heappop(heap))
+        else:
+            print(0)
+    else:
+        hq.heappush(heap,-x)
