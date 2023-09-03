f85db11b94b750a45da76753ea37e497c05cf835
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-09-03 14:55:44 +0900

    fix: 프로그래머스 이중우선순위큐 문제 풀이 수정

--- jiyoung00000/3주차/pro_이중우선순위큐.py
+++ jiyoung00000/3주차/pro_이중우선순위큐.py
@@ -1,34 +1,29 @@
-# 실패 코드..
-# 테스트 1
-# 입력값 〉	["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
-# 기댓값 〉	[0, 0]
-# 실행 결과 〉	실행한 결괏값 [123,123]이 기댓값 [0,0]과 다릅니다.
-import heapq
+import heapq 
 
-def solution(operations):
-    max_heap = [] 
-    min_heap = [] 
+def solution(operations): 
+    max_heap = [] # 최대 힙
+    min_heap = [] # 최소 힙
 
-    for operation in operations:
-        cmd, num = operation.split()
-        num = int(num)
+    for operation in operations: # 연산 수행
+        cmd, num = operation.split() # 명령어, 숫자 분리
+        num = int(num) # 숫자는 정수형으로 변환
         
-        if cmd == "I": 
-            heapq.heappush(max_heap, -num) 
-            heapq.heappush(min_heap, num) 
-        elif cmd == "D" and num == 1: 
-            if max_heap: 
-                heapq.heappop(max_heap) 
-                if len(max_heap) == 0 or -max_heap[0] < min_heap[0]:
-                    min_heap = []
-                    max_heap = []
-        elif cmd == "D" and num == -1: 
-            if min_heap: 
-                heapq.heappop(min_heap) 
-                if len(min_heap) == 0 or -max_heap[0] < min_heap[0]:
-                    min_heap = []
-                    max_heap = []   
-
+        if cmd == "I": # 삽입
+            heapq.heappush(max_heap, -num) # 최대 힙에 삽입
+            heapq.heappush(min_heap, num) # 최소 힙에 삽입
+        elif cmd == "D" and num == 1: # 최대값 삭제
+            if max_heap: # 최대 힙이 비어있지 않다면
+                heapq.heappop(max_heap) # 최대 힙에서 최대값 삭제
+                if len(max_heap) == 0 or -max_heap[0] < min_heap[0]: # 최대 힙이 비어있거나 최대 힙의 최대값이 최소 힙의 최소값보다 작다면
+                    min_heap = [] # 최소 힙 초기화
+                    max_heap = [] # 최대 힙 초기화
+        elif cmd == "D" and num == -1: # 최소값 삭제
+            if min_heap: # 최소 힙이 비어있지 않다면
+                heapq.heappop(min_heap) # 최소 힙에서 최소값 삭제
+                if len(min_heap) == 0 or -max_heap[0] < min_heap[0]: # 최소 힙이 비어있거나 최대 힙의 최대값이 최소 힙의 최소값보다 작다면
+                    max_heap = [] # 최대 힙 초기화
+                    min_heap = [] # 최소 힙 초기화 
+                    
     if len(min_heap) == 0:
         return [0, 0]
     else:
